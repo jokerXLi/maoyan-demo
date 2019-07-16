e5ed4ca5af06edf270a7eed637ea32ec040e61d5
lixinglong <lixinglong@qq.com>
2019-07-16 10:18:46 +0800

    底部导航完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
-import APP from "./APP";
 import router from './router';
+import APP from "./APP";
 import store from 'store';
+import "./asstes/styles/base.scss";
 //后缀可省略；
 
 new Vue({
   el: "#app",
-  router:router,
-  store:store,
+  router,
+  store,
   render: h => h(APP)
 });
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,13 +2,41 @@
   <div class="page-home">
     <router-view></router-view>
     <h1>电影首页，个人中心页，影院列表页</h1>
+    <!-- 引入组件 -->
+    <Tabbar
+    :tabs="[
+    { name: '电影', icon: 'icondianying', to: '/films' },
+      { name: '影院', icon: 'iconyingyuan', to: '/cinemas' },
+      { name: '我的', icon: 'iconwode1', to: '/center' },
     
+    ]"
+    >
+
+
+
+    </Tabbar>
   </div>
 </template>
 
 
 <script>
+import Tabbar from "@/components/Tabbar/index.vue"; //@等于根目录
 export default {
-  name:'home'
-}
+  name: "home",
+  //注册组件
+  components: {
+    Tabbar //组件名
+  }
+};
 </script>
+<style lang="scss">
+.page-home {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  > div:first-child {
+    flex: 1;
+    overflow-y: auto;
+  }
+}
+</style>
